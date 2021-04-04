67b8d4d7886e1fb74cade05ba64a31889c39b8c1
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2021-04-04 21:17:41 +0200

    initial commit
    working

---
+++ 2.Project_List.py
@@ -0,0 +1,36 @@
+# =============================================================================
+# Average Word Length
+# 
+# 
+# Given a sentence as input, calculate and output 
+# the average word length of that sentence.
+# To calculate the average word length, you need to divide the sum of all
+# word lengths by the number of words in the sentence.
+# 
+# Sample Input:
+# this is some text
+# 
+# Sample Output:
+# 3.5
+# 
+# Explanation: There are 4 words in the given input,
+# with a total of 14 letters, so the average length will be: 14/4 = 3.5
+# Strings have a split() method, which returns the string split into a list, 
+# with the given separator. By default, the separator is a space, 
+# so calling split() will return a list containing the words of the string as 
+# items.
+# =============================================================================
+
+
+text = "Project 2 test tekst istedenfor input fra user"
+#text = input()
+
+text2 = text.split()
+tot = 0 
+for i in text:
+    if i != " ":
+        tot +=1
+
+
+text3 = (len(text2))
+print(tot/text3)
